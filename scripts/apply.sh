b2dc33374f67bef5fb69b9a48f9eeb87710d1ecc
Tommy Speigner <tspeigner@gmail.com>
2018-09-13 10:08:51 -0700

    removing scripts

--- scripts/apply.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -e "AWS_ACCESS_KEY_ID=AKIAJWEOPNHBF4C5GBKQ" -e "AWS_SECRET_ACCESS_KEY=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" -v $(pwd):/app/ -w /app/ hashicorp/terraform:light apply --var-file=tommy.varfile
