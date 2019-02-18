ae8f51c710f0e8ebfb7c69082171b40736f8efc2
mtyates <matthewtyleryates@gmail.com>
2019-02-18 11:03:57 -0500

    Update provider.tf
    Customizing to my needs.

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@
 #--------------------------------------------------------------
 provider "aws" {
   region                  = "${var.region}"
-  shared_credentials_file = "/Users/tommy/.aws/credentials"
+  shared_credentials_file = "/Users/matthew.yates/.aws/credentials"
 }
